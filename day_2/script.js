2e5c23a3255926e02bd56fff2dc3e8465a065bc1
ryezzz <ryezzz@gmail.com>
2018-06-03 01:07:44 +0000

    update

--- day_2/script.js
+++ day_2/script.js
@@ -1,48 +1,48 @@
 var parseTime = d3.timeParse("%Y-%m-%d")
 var monthParse = d3.timeParse("%m")
 var yearParse = d3.timeParse("%Y")
-    
-d3.dsv(',', 'data/TOTALSA.csv', function(d) {
-         
 
-        var split = d.DATE.split("-")
-        var year = split[0]
-        return {
-            date: parseTime(d.DATE),
-            year: yearParse(+split[0]),
-            month: monthParse(+split[1]),
-            totalSA: +d.TOTALSA
-        }
-        
-    }).then(function(data) {
-//Render chart/legend/everything that has an SVG on first load and resize    
-        $(window).on('resize', function() {
-            d3.selectAll('svg')
-              .remove()
-            renderHeatmap(data)
-        });
-    
+d3.dsv(',', 'data/TOTALSA.csv', function(d) {
+
+
+    var split = d.DATE.split("-")
+    var year = split[0]
+    return {
+        date: parseTime(d.DATE),
+        year: yearParse(+split[0]),
+        month: monthParse(+split[1]),
+        totalSA: +d.TOTALSA
+    }
+
+}).then(function(data) {
+    //Render chart/legend/everything that has an SVG on first load and resize    
+    $(window).on('resize', function() {
+        d3.selectAll('svg')
+            .remove()
         renderHeatmap(data)
+    });
+
+    renderHeatmap(data)
 });
 
 
 function renderHeatmap(data) {
 
 
-//variables for the svgs. These need to be created in the render function rather than globally beause of my remove method   
+    //variables for the svgs. These need to be created in the render function rather than globally beause of my remove method   
     var chartSVG = d3.selectAll('.chartWrapper').append('svg'),
         legendSvg = d3.selectAll('.legendWrapper').append('svg'),
-        width = function returnWidth(itemToMeasure) {return parseInt(d3.select(itemToMeasure.node().parentNode).style('width'))},
-        height = function returnWidth(itemToMeasure) {return parseInt(d3.select(itemToMeasure.node().parentNode).style('height'))},
+        width = function returnWidth(itemToMeasure) { return parseInt(d3.select(itemToMeasure.node().parentNode).style('width')) },
+        height = function returnHeight(itemToMeasure) { return parseInt(d3.select(itemToMeasure.node().parentNode).style('height')) },
         widthChart = width(chartSVG),
-        heightChart = height(chartSVG),
+        heightChart = Math.floor(height(chartSVG)),
         widthLegend = width(legendSvg)
-        
-//Responsive Variables
+
+    //Responsive Variables
     var tickAttributes = new Object(),
         legendAttributes = new Object(),
         chartAttributes = new Object()
-        chartAttributes.rectHeight = heightChart/12;
+    chartAttributes.rectHeight = heightChart / 12;
 
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) | window.innerWidth < 600) {
         chartAttributes.rectWidth = widthChart / 12
@@ -52,27 +52,29 @@ function renderHeatmap(data) {
         chartAttributes.rectWidth = widthChart / 12
         tickAttributes.text_kerning = 50
     }
-    
-    // var timeScale = d3.scaleTime()
-    // console.log(timeScale(d3.extent(data, function(d) { return d.month })))
-        
-        
-    var yDomain = d3.extent(data, function(d) {return d.month}),
-        
-        xDomain = d3.extent(data, function(d){return d.year}),
-                
-        yScaleChart =  d3.scaleTime()
-                      .domain(yDomain)
-                      .rangeRound([0, heightChart]),
-        xScaleChart = d3.scaleTime()
-                      .domain(xDomain)
-                      .rangeRound([0, widthChart]),
-        
-         colorScaleChart = d3.scaleLinear()
-                            .domain(d3.extent(data, function(d) {return d.totalSA}))
-                            .range([0, 1]);
 
-//First, render the chart with the responsive variables defined above
+
+    var yDomain = d3.extent(data, function(d) { return d.month }),
+
+        xDomain = d3.extent(data, function(d) { return d.year }),
+
+        testArr = d3.map(data, function(d) { return d.year; }).keys(),
+        monthArr = d3.map(data, function(d) { return d.month; }).keys(),
+
+        yScaleChart = d3.scalePoint()
+        .domain(monthArr)
+        .range([0, heightChart]),
+        xScaleChart = d3.scalePoint()
+        .domain(testArr)
+        .range([0, widthChart]),
+
+
+        colorScaleChart = d3.scaleLinear()
+        .domain(d3.extent(data, function(d) { return d.totalSA }))
+        .range([0, 1]);
+
+
+    //First, render the chart with the responsive variables defined above
     function renderChart() {
 
         chartSVG.attr('width', widthChart)
@@ -80,26 +82,40 @@ function renderHeatmap(data) {
         var formatYear = d3.timeFormat("%Y")
         var formatMonth = d3.timeFormat("%m")
         console.log(xDomain)
-        var rectWidth = formatYear(xDomain[1])-formatYear(xDomain[0]),
-            rectHeight = formatMonth(yDomain[1])-formatMonth(yDomain[0])
 
-        
+        var widthRect = d3.scaleBand()
+            .domain(testArr)
+            .range([0, widthChart])
+            .padding([.1])
+            .paddingInner([0])
+
+        var heightRect = d3.scaleBand()
+            .domain([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+            .range([0, heightChart + 1])
+            .padding([.1])
+            .paddingInner([0])
+
+
         chartSVG.selectAll('rect')
             .data(data)
             .enter()
             .append('rect')
-            .attr('width', widthChart/rectWidth)
-            .attr('height',  heightChart/rectHeight)
-            .attr('y', function(d){ return yScaleChart(d.month)})
-            .attr('x', function(d){ return xScaleChart(d.year)})
-            .attr('fill', function(d){return 'orange'})
-            .attr('opacity', function(d){return colorScaleChart(d.totalSA)})
+            .attr('width', function(d) { return widthRect.bandwidth() })
+            .attr('height', function(d) { return heightRect.bandwidth() })
+
+            // .attr('height', function(d){return Math.floor(heightChart/monthArr.length)})
+            .attr('y', function(d, i) { return yScaleChart(d.month) })
+            .attr('x', function(d, i) { return xScaleChart(d.year) })
+
+            // .attr('x', function(d, i) {return i * (widthChart / testArr.length); })
+            .attr('fill', function(d) { return 'orange' })
+            .attr('opacity', function(d) { return colorScaleChart(d.totalSA) })
     }
 
     renderChart()
 
 
-//Second, render the legend
+    //Second, render the legend
     function renderLegend() {
 
 
